2f2dfa523c83a583f8a927d774dc629fbadc20e5
Alexis260698 <hena260698@gmail.com>
2024-01-03 16:41:13 -0600

    maquetandop formulario

--- src/Components/UserForm.jsx
+++ src/Components/UserForm.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react"
+
+const initialUserForm = {
+    username: '',
+    password: '',
+    email: '',
+}
 
 export const UserForm = () => {
-    return (
 
-        <form></form>
+    const [userForm, setUserForm] = useState(initialUserForm);
+
+    const { username, password, email } = userForm;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setUserForm({
+            ...userForm,
+            [name]: value
+        })
+    }
+
+
+    return (
+        <form>
+            <input
+                className="form-control my-3 w-75"
+                placeholder="Username"
+                name="username"
+                value={username} onChange={onInputChange} />
+            <input
+                className="form-control my-3 w-75"
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={onInputChange} />
+            <input
+                className="form-control my-3 w-75"
+                placeholder="email"
+                name="email"
+                value={email}
+                onChange={onInputChange} />
+            <button
+                className="btn btn-primary"
+                type='submit'
+            >Crear</button>
+        </form>
 
     )
 }
